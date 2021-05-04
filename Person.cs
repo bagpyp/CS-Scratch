414d2a30bf22d45a738b154676fc2dc831a0439f
bagpyp <robert@bagpyp.net>
2021-05-04 01:37:34 -0700

    initial commit

---
+++ Person.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+
+namespace Scratch 
+{
+    public class Person 
+    {
+        // attributes
+        public string Name { get; set; }
+        public List<Person> Friends { get; set; }
+
+        // methods
+        public void Introduce() 
+        {
+            Console.WriteLine($"My name is {this.Name},");
+            if (this.Friends != null) 
+            {
+                System.Console.WriteLine("My friends are");
+                this.Friends.ForEach(f => Console.WriteLine($"\t{f.Name}"));
+            } 
+            else 
+            {
+                System.Console.WriteLine("I have no friends...");
+            }
+        }
+        public void Befriend(Person p) 
+        {
+            if (this.Friends == null) 
+                this.Friends = new List<Person>() {p};
+            else if (this.Friends.Contains(p))
+                {}
+            else
+                this.Friends.Add(p);
+        }
+        public void Unfriend(Person p) 
+        {   
+            if (this.Friends == null) 
+                {}
+            else if (this.Friends.Contains(p))
+                this.Friends.Remove(p);
+            else
+                {}
+        }
+
+        public static void CreateFriendship(Person p1, Person p2) 
+        {
+            p1.Befriend(p2);
+            p2.Befriend(p1);
+        }
+        public static void EndFriendship(Person p1, Person p2) 
+        {
+            p1.Unfriend(p2);
+            p2.Unfriend(p1);
+        }
+        // constructors
+        public Person()
+        {
+            this.Name = "Stranger";
+        }
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Scratch
+{    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person("Robbie");
+            var p2 = new Person("Heather");
+            p2.Introduce();
+        }
+    }
+}
